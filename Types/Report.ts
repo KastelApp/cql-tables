151b2918a6e911e797525d289d8cb18e50e82599
Darker-Ink <rstonehouse66@gmail.com>
2023-10-06 12:17:09 -0500

    fix: changing js to ts (and fixing report)

--- Types/Report.ts
+++ Types/Report.ts
@@ -1,12 +1,12 @@
 interface Report {
-    Type: number; // 0 = user, 1 = guild, 2 = message, 3 = other
-    Reporter: string;
-    Flags: number;
-    ReportedId: string; // The id of the thing that was reported (user, guild, message, etc)
-    ReportId: string; // The id of the report
-    AssignedTo: string; // The staff member assigned to the report
-    Status: number;
+    AssignedTo: string; // The staff member assigned to the report 
     CreatedAt: Date;
+    Flags: number;
+    ReportId: string;  // The id of the report
+    ReportedId: string; // The id of the thing that was reported (user, guild, message, etc)
+    Reporter: string;
+    Status: number;
+    Type: number; // 0 = user, 1 = guild, 2 = message, 3 = other
     UpdatedAt: Date;
 }
 
