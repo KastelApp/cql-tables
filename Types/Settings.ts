648fa3e3a5a959848aaef8a23718147155b41fda
Darker-Ink <rstonehouse66@gmail.com>
2024-03-17 08:21:58 -0500

    feat: new settings

--- Types/Settings.ts
+++ Types/Settings.ts
@@ -21,11 +21,13 @@ interface Settings {
 	allowedInvites: number,
 	bio: string | null;
 	customStatus: string | null;
+	emojiPack: "fluentui-emoji" | "native" | "noto-emoji" | "twemoji";
 	guildOrder: GuildOrder[] | null,
 	language: string;
 	maxFileUploadSize: number;
 	maxGuilds: number;
 	mentions: SettingsMentions[];
+	navLocation: "bottom" | "left";
 	privacy: number;
 	status: number;
 	theme: string;
