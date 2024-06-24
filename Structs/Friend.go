3e6438a87cd13683d946ba6f00616b8dc3ec92a9
Darker-Ink <rstonehouse66@gmail.com>
2024-06-24 18:58:16 -0500

    chore: redoing friend table again

--- Structs/Friend.go
+++ Structs/Friend.go
@@ -8,17 +8,18 @@ import (
 
 var FriendsTable = table.New(table.Metadata{
 	Name:    "friends",
-	Columns: []string{"created_at", "flags", "friend_id", "primary_user_id", "primary_user_nickname", "secondary_user_id", "secondary_user_nickname"},
-	PartKey: []string{"friend_id"},
+	Columns: []string{"created_at", "friend_id", "primary_user_flags", "primary_user_id", "primary_user_nickname", "secondary_user_flags", "secondary_user_id", "secondary_user_nickname"},
+	PartKey: []string{"friend_id", "primary_user_id", "secondary_user_id"},
 	SortKey: []string{},
 })
 
 type Friends struct {
 	CreatedAt            time.Time `db:"created_at"`
-	Flags                int    `db:"flags"`
 	FriendID             string `db:"friend_id"`
+	PrimaryUserFlags     int `db:"primary_user_flags"`
 	PrimaryUserID        string `db:"primary_user_id"`
 	PrimaryUserNickname  string `db:"primary_user_nickname"`
+	SecondaryUserFlags   int `db:"secondary_user_flags"`
 	SecondaryUserID      string `db:"secondary_user_id"`
 	SecondaryUserNickname string `db:"secondary_user_nickname"`
 }
--- Types/Friend.ts
+++ Types/Friend.ts
@@ -1,11 +1,14 @@
 interface Friends {
 	createdAt: Date,
-	flags: number;
 	friendId: string;
-	primaryUserId: string;
-	primaryUserNickname: string; // ? The secondary user sets this
+	primaryUserFlags: number;
+	primaryUserId: string; 
+	primaryUserNickname: string;
+	// ? The primary user sets this
+	secondaryUserFlags: number; 
+	// ? The secondary user sets this
 	secondaryUserId: string;
-	secondaryUserNickname: string; // ? The primary user sets this
+	secondaryUserNickname: string;
 }
 
 export default Friends;
